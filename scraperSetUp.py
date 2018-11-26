bd29c04a917a8d096056217ce18ee5101dd80d91
Kelvin <44570273+kks110@users.noreply.github.com>
2018-11-26 23:42:17 +0000

    Update scraperSetUp.py

--- scraperSetUp.py
+++ scraperSetUp.py
@@ -1,11 +1,11 @@
 
 
 def scraperSetUp():
-    #Sets up the loop
+    #Sets up the loop.
     while True:
-        #Asks for the set code
+        #Asks for the set code.
         set_input = input("What is the set code for the set? - ")
-        #Opens the base file and creates a new file using the set name
+        #Opens the base file and creates a new file using the set name.
         f1 = open('mtg_cardPull.py', 'r')
         f2 = open(set_input + '_cardPull.py', 'w')
         for line in f1:
@@ -13,10 +13,10 @@ def scraperSetUp():
         f1.close()
         f2.close()
         created_file = set_input + '_cardPull'
-        #Runs the created file
+        #Runs the created file.
         data_scraper = __import__(created_file)
         data_scraper.main()
-        #Asks is there is another set that needs to be scraped
+        #Asks is there is another set that needs to be scraped.
         another_set = input("Do you have another set you need to download? (y/n) ")
         if another_set.lower() =="y":
             continue
