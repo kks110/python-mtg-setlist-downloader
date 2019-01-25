e99ee9132b57695ac22b8bf181f5b4ea9fd7e4dd
Kelvin <44570273+kks110@users.noreply.github.com>
2019-01-25 19:54:38 +0100

    Add files via upload
    changed the code so if there are over 350 cards, it will download them all.
    Fixed a bug where, if there was a number in the set name, it would fail.
    Fixed a bug where, if the set had no token or promos, it would fail.

--- mtg_cardPull.py
+++ mtg_cardPull.py
@@ -5,12 +5,10 @@ import requests
 #Used to export the data to a CSV
 import csv
 
-#This opens and writes to the file.
-def write_file(abc_json, set_count, inc_prerelease, set_type="0"):
-    #Opens the file.
-    with open('abc.csv', 'a', newline='', encoding='utf8') as csv_file:
+#This writes the file.
+def write_file(set_json, set_count, inc_prerelease, set_type="0"):
+    with open('xyz.csv', 'a', newline='', encoding='utf8') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
-        #Dependant on set type, will enter info in to the sheet to distinguish the different sections.
         if set_type == "":
             writer.writerow(["Number", "Card Name", "Colour", "Rarity"])
         elif set_type == "t":
@@ -18,12 +16,11 @@ def write_file(abc_json, set_count, inc_prerelease, set_type="0"):
         elif set_type == "p":
             writer.writerow(["Promos"])
         for Counter in range(set_count):
-            #Loops through the cards and puts them in to the CSV file.
-            card_name = abc_json['data'][Counter]['name']
-            rarity = abc_json['data'][Counter]['rarity']
-            colour = abc_json['data'][Counter]['color_identity']
-            collector_number = abc_json['data'][Counter]['collector_number']
-            #If you want to include pre-release promos, it will write the row depending on if there is an S next to the number.
+            #Prints the data out on screen and also puts it in to the CSV file
+            card_name = set_json['data'][Counter]['name']
+            rarity = set_json['data'][Counter]['rarity']
+            colour = set_json['data'][Counter]['color_identity']
+            collector_number = set_json['data'][Counter]['collector_number']
             if inc_prerelease:
                 writer.writerow([collector_number, card_name, colour, rarity])
             else:
@@ -31,37 +28,38 @@ def write_file(abc_json, set_count, inc_prerelease, set_type="0"):
                     writer.writerow([collector_number, card_name, colour, rarity])
 
 def api_call(set_type, inc_prerelease):
-    #API call to get the JSON file from Scryfall.
-    abc_api = "https://api.scryfall.com/sets/" + set_type + "abc"
-    response = requests.get(abc_api)
-    abc_json = response.json()
-    #The fist JSON doesn't have the info on the card, but it gives the search_uri to get the details.
-    abc_details = abc_json['search_uri']
-    full_response = requests.get(abc_details)
-    abc_full_json = full_response.json()
-    total_cards = abc_full_json['total_cards']
-    #Counts the amount of cards in the set.
-    set_count_stage = abc_full_json['data']
-    set_count = len(set_count_stage)
-    #Calls the function to write to the CSV file.
-    write_file(abc_full_json, set_count, inc_prerelease, set_type)
-    #If there are more than 175 cards, code to get the second half. The JSONs only contain info for the first 175 cards.
-    if set_count == 175:
-        abc_continued = abc_full_json['next_page']
-        full_response_cont = requests.get(abc_continued)
-        abc_cont_json = full_response_cont.json()
-        write_file(abc_cont_json, total_cards - 175, inc_prerelease)
+    card_counter = 0
+    #API call to get the JSON file
+    set_api = "https://api.scryfall.com/sets/" + set_type + "xyz"
+    response = requests.get(set_api)
+    set_json = response.json()
+    if set_json['object'] != "error":
+        set_details = set_json['search_uri']
+        full_response = requests.get(set_details)
+        set_full_json = full_response.json()
+        total_cards = set_full_json['total_cards']
+        #Counts the amount of cards in the set
+        set_count = len(set_full_json['data'])
+        #Creates the CSV file
+        write_file(set_full_json, set_count, inc_prerelease, set_type)
+        card_counter += set_count
+        #If there are more than 175 cards, code to get the second half. The JSONs only contain info for the first 175 cards
+        while card_counter < total_cards:
+            continued = set_full_json['next_page']
+            full_response_cont = requests.get(continued)
+            set_full_json = full_response_cont.json()
+            set_count = (len(set_full_json['data']))
+            write_file(set_full_json, set_count, inc_prerelease)
+            card_counter += set_count
 
 
 def setup():
-    #Will ask if you want to include the pre-release promo cards.
-    inc_prerelease = input("Would you like to include pre-release promos? (y/n) ")
+    inc_prerelease = input("Would you like to include pre-release promos? (y/N) ")
     if inc_prerelease.lower() == "y":
         inc_prerelease = True
         print("Card that are pre-release promos will appear in the promo section, with an S next to their number")
     else:
         inc_prerelease = False
-    #Sets the aditional part of the straing needed for the API URL.
     set_main = ""
     set_token = "t"
     set_promo = "p"
--- scraperSetUp.py
+++ scraperSetUp.py
@@ -1,23 +1,23 @@
 
 
 def scraperSetUp():
-    #Sets up the loop.
+    #Sets up the loop
     while True:
-        #Asks for the set code.
+        #Asks for the set code
         set_input = input("What is the set code for the set? - ")
-        #Opens the base file and creates a new file using the set name.
+        #Opens the base file and creates a new file using the set name
         f1 = open('mtg_cardPull.py', 'r')
         f2 = open(set_input + '_cardPull.py', 'w')
         for line in f1:
-            f2.write(line.replace('abc', set_input))
+            f2.write(line.replace('xyz', set_input))
         f1.close()
         f2.close()
         created_file = set_input + '_cardPull'
-        #Runs the created file.
+        #Runs the created file
         data_scraper = __import__(created_file)
         data_scraper.main()
-        #Asks is there is another set that needs to be scraped.
-        another_set = input("Do you have another set you need to download? (y/n) ")
+        #Asks is there is another set that needs to be scraped
+        another_set = input("Do you have another set you need to download? (y/N) ")
         if another_set.lower() =="y":
             continue
         else:
