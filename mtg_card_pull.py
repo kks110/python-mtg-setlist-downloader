9da306ec713ca6dcea078c63169aaab0c8aaf063
Kelvin Samuel <kelvin@kks110.com>
2020-01-03 20:38:21 +0000

    refactoriong

--- mtg_card_pull.py
+++ mtg_card_pull.py
@@ -1,13 +1,12 @@
-#Used for processing JSONS files
-import json
-#Used to get the URL
+# Used to get the URL
 import requests
-#Used to export the data to a CSV
+# Used to export the data to a CSV
 import csv
 
-#This writes the file.
-def write_file(set_json, set_count, inc_prerelease, set_type="0"):
-    with open('xyz.csv', 'a', newline='', encoding='utf8') as csv_file:
+
+# This writes the file.
+def write_file(mtg_set, set_json, set_count, inc_prerelease, set_type="c"):
+    with open(mtg_set + '.csv', 'a', newline='', encoding='utf8') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         if set_type == "":
             writer.writerow(["Number", "Card Name", "Colour", "Rarity"])
@@ -16,7 +15,7 @@ def write_file(set_json, set_count, inc_prerelease, set_type="0"):
         elif set_type == "p":
             writer.writerow(["Promos"])
         for x in range(set_count):
-            #Prints the data out on screen and also puts it in to the CSV file
+            # Appends data to the CSV file
             card_name = set_json['data'][x]['name']
             rarity = set_json['data'][x]['rarity']
             colour = set_json['data'][x]['color_identity']
@@ -27,50 +26,39 @@ def write_file(set_json, set_count, inc_prerelease, set_type="0"):
                 if "s" not in collector_number:
                     writer.writerow([collector_number, card_name, colour, rarity])
 
-def api_call(set_type, inc_prerelease):
+
+def api_call(mtg_set, set_type, inc_prerelease):
     card_counter = 0
-    #API call to get the JSON file
-    set_api = "https://api.scryfall.com/sets/" + set_type + "xyz"
-    response = requests.get(set_api)
-    set_json = response.json()
+    # API call to get the JSON file
+    set_api = "https://api.scryfall.com/sets/" + set_type + mtg_set
+    set_json = requests.get(set_api).json()
     if set_json['object'] != "error":
         set_details = set_json['search_uri']
-        full_response = requests.get(set_details)
-        set_full_json = full_response.json()
+        set_full_json = requests.get(set_details).json()
         total_cards = set_full_json['total_cards']
-        #Counts the amount of cards in the set
+        # Counts the amount of cards in the set
         set_count = len(set_full_json['data'])
-        #Creates the CSV file
-        write_file(set_full_json, set_count, inc_prerelease, set_type)
+        # Creates the CSV file
+        write_file(mtg_set, set_full_json, set_count, inc_prerelease, set_type)
         card_counter += set_count
-        #If there are more than 175 cards, code to get the second half. The JSONs only contain info for the first 175 cards
+        # If there are more than 175 cards, code to get the second half.
+        # The JSONs only contain info for the first 175 cards
         while card_counter < total_cards:
             continued = set_full_json['next_page']
-            full_response_cont = requests.get(continued)
-            set_full_json = full_response_cont.json()
+            set_full_json = requests.get(continued).json()
             set_count = (len(set_full_json['data']))
-            write_file(set_full_json, set_count, inc_prerelease)
+            write_file(mtg_set, set_full_json, set_count, inc_prerelease)
             card_counter += set_count
-
-
-def setup():
-    inc_prerelease = input("Would you like to include pre-release promos? (y/N) ")
-    if inc_prerelease.lower() == "y":
-        inc_prerelease = True
-        print("Card that are pre-release promos will appear in the promo section, with an S next to their number")
     else:
-        inc_prerelease = False
+        print("An error has occured. Maybe the set code was wrong?")
+
+
+def setup(mtg_set, inc_prerelease):
     set_main = ""
     set_token = "t"
     set_promo = "p"
-    api_call(set_main, inc_prerelease)
-    api_call(set_token, inc_prerelease)
-    api_call(set_promo, inc_prerelease)
+    api_call(mtg_set, set_main, inc_prerelease)
+    api_call(mtg_set, set_token, inc_prerelease)
+    api_call(mtg_set, set_promo, inc_prerelease)
 
-def main():
-    setup()
-
-#This starts my program!
-if __name__ == "__main__":
-    main()
 
--- scraper_setup.py
+++ scraper_setup.py
@@ -1,23 +1,32 @@
+import requests
+import mtg_card_pull
+
+
+def valid_set(mtg_set):
+    set_api = "https://api.scryfall.com/sets/" + mtg_set
+    set_json = requests.get(set_api).json()
+    if set_json['object'] == "error":
+        print(mtg_set + " is an invalid set name")
+        return False
+    else:
+        return True
 
 
 def scraper_setup():
-    #Sets up the loop
+    # Sets up the loop
     while True:
-        #Asks for the set code
+        # Asks for the set code
         set_input = input("What is the set code for the set? - ")
-        #Opens the base file and creates a new file using the set name
-        f1 = open('mtg_card_pull.py', 'r')
-        f2 = open(set_input + '_card_pull.py', 'w')
-        for line in f1:
-            f2.write(line.replace('xyz', set_input))
-        f1.close()
-        f2.close()
-        created_file = set_input + '_card_pull'
-        #Runs the created file
-        data_scraper = __import__(created_file)
-        data_scraper.main()
-        #Asks is there is another set that needs to be scraped
-        another_set = input("Do you have another set you need to download? (y/N) ")
+        if valid_set(set_input):
+            inc_prerelease = input("Would you like to include pre-release promos? (y/n) ")
+            if inc_prerelease.lower() == "y":
+                inc_prerelease = True
+                print("Card that are pre-release promos will appear in the promo section, with an S next to their number")
+            else:
+                inc_prerelease = False
+            mtg_card_pull.setup(set_input, inc_prerelease)
+        # Asks is there is another set that needs to be scraped
+        another_set = input("Do you have another set you need to download? (y/n) ")
         if another_set.lower() =="y":
             continue
         else:
@@ -27,6 +36,7 @@ def scraper_setup():
 def main():
     scraper_setup()
 
-#This starts my program!
+
+# This starts my program!
 if __name__ == "__main__":
     main()
